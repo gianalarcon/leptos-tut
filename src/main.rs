ef3e1c95aef59d387e4c76537068095393a0dfca
gianalarcon <gian.alarcon@unmsm.edu.pe>
2024-01-26 19:23:09 -0500

    Feat: Improve ProgressBar

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use leptos::*;
 fn main() {
     //console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <App/> })
+    mount_to_body(App)
 }
 
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
-    //let double_count = move || count() * 2;
+    let double_count = move || count() * 2;
     view! {
         <button on:click=move |_| {
             set_count.update(|n| *n += 1);
@@ -19,14 +19,16 @@ fn App() -> impl IntoView {
         </button>
 
         <br/>
+        <ProgressBar max=50 progress=count/>
         <ProgressBar progress=count/>
+        <ProgressBar max=50 progress=Signal::derive(double_count)/>
     }
 }
 
 #[component]
-fn ProgressBar<F: Fn() -> i32 + 'static>(
+fn ProgressBar(
     #[prop(default = 100)] max: u16,
-    progress: F,
+    #[prop(into)] progress: Signal<i32>,
 ) -> impl IntoView {
     view! {
         <progress
